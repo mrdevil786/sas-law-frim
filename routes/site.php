b424a7f5fa2859c70a96a19ba4da08f97a6f020d
mrdevil786 <suhailsaeedme@gmail.com>
2024-08-10 15:41:23 +0530

    Initial Commit

--- routes/site.php
+++ routes/site.php
@@ -4,7 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\BasicController;
 
 Route::get('/',[BasicController::class,'dashboard'])->name('site.dashboard');
-
-Route::get('/about', function () {
-    return 'This is about page.';
-});
