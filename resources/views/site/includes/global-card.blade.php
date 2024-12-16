82f6411ea672810f62848657d8441c0a2126b3c1
Muba9161 <khanmuba3@gmail.com>
2024-12-16 23:18:06 +0530

    Share button updated

--- resources/views/site/includes/global-card.blade.php
+++ resources/views/site/includes/global-card.blade.php
@@ -9,7 +9,8 @@
                 </a>
             </div>
             <div class="blog_details">
-                <a class="d-inline-block" href="{{ !empty($routePrefix) ? route($routePrefix, ['slug' => $data->slug]) : '#' }}">
+                <a class="d-inline-block"
+                    href="{{ !empty($routePrefix) ? route($routePrefix, ['slug' => $data->slug]) : '#' }}">
                     <h2>{{ \Illuminate\Support\Str::limit($data->title, 20, '...') }}</h2>
                 </a>
                 <p>{{ \Illuminate\Support\Str::limit(strip_tags($data->description), 99, '...') }}</p>
@@ -17,30 +18,36 @@
                     @if (!empty($data->author->name))
                         <li><a href="#"><i class="fa fa-user"></i> {{ $data->author->name }}</a></li>
                     @endif
-                    <li><a href="#"><i class="fa fa-calendar"></i> {{ $data->created_at->format('d M Y') }}</a></li>
+                    <li><a href="#"><i class="fa fa-calendar"></i> {{ $data->created_at->format('d M Y') }}</a>
+                    </li>
                 </ul>
                 <!-- Share Button Section -->
                 <div class="share-buttons mt-3">
-                    <button class="btn btn-sm btn-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-share-alt"></i> Share
-                    </button>
+                    <button class="btn btn-sm btn-primary" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"> <i class="fas fa-share-alt"></i> Share </button>
                     <div class="dropdown-menu">
                         <!-- Share to WhatsApp -->
-                        <a class="dropdown-item" href="https://api.whatsapp.com/send?text={{ urlencode($data->title . ' - ' . route($routePrefix, ['slug' => $data->slug])) }}" target="_blank">
-                            <i class="fab fa-whatsapp" style="color: red;"></i> WhatsApp
+                        <a class="dropdown-item py-3"
+                            href="https://api.whatsapp.com/send?text={{ urlencode($data->title . ' - ' . route($routePrefix, ['slug' => $data->slug])) }}" target="_blank">
+                            <i class="fab fa-whatsapp" style="color: #25D366;"></i> WhatsApp
                         </a>
                         <!-- Share to Facebook -->
-                        <a class="dropdown-item" href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route($routePrefix, ['slug' => $data->slug])) }}" target="_blank">
-                            <i class="fab fa-facebook" style="color: red;"></i> Facebook
-                        </a>
+                        <a class="dropdown-item py-3"
+                            href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route($routePrefix, ['slug' => $data->slug])) }}" target="_blank">
+                            <i class="fab fa-facebook" style="color: #1877F2;"></i> Facebook</a>
                         <!-- Share to Instagram (Instagram doesn't allow direct sharing like other platforms) -->
-                        <a class="dropdown-item" href="https://www.instagram.com/" target="_blank">
-                            <i class="fab fa-instagram" style="color: red;"></i> Instagram
-                        </a>
+                        <!-- Share to Instagram (Mobile approach - Instagram app) -->
+                        <a class="dropdown-item py-3"
+                            href="instagram://share?text={{ urlencode($data->title . ' - ' . route($routePrefix, ['slug' => $data->slug])) }}" target="_blank">
+                            <i class="fab fa-instagram" style="color: #c13584"></i> Instagram</a>
+                        <!-- Share to LinkedIn -->
+                        <a class="dropdown-item py-3"
+                            href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(route($routePrefix, ['slug' => $data->slug])) }}&title={{ urlencode($data->title) }}" target="_blank">
+                            <i class="fab fa-linkedin" style="color: #0077b5;"></i> LinkedIn</a>
                         <!-- Copy Link Button -->
-                        <a class="dropdown-item" href="javascript:void(0);" onclick="copyToClipboard('{{ route($routePrefix, ['slug' => $data->slug]) }}')">
-                            <i class="fas fa-link" style="color: red;"></i> Copy Link
-                        </a>
+                        <a class="dropdown-item py-3" href="javascript:void(0);"
+                            onclick="copyToClipboard('{{ route($routePrefix, ['slug' => $data->slug]) }}')">
+                            <i class="fas fa-link" style="color: #C0C0C0;"></i> Copy Link</a>
                     </div>
                 </div>
             </div>
